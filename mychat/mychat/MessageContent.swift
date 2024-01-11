33c3ccffa3bae47a6e3d88ed6743970f73603a59
Jose Morales <moralesjose1428@gmail.com>
2024-01-10 21:18:55 -0800

    began message UI

---
+++ mychat/mychat/MessageContent.swift
@@ -0,0 +1,18 @@
+//
+//  MessageContent.swift
+//  mychat
+//
+//  Created by Jose Morales on 1/10/24.
+//
+
+import SwiftUI
+
+struct MessageContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageContent()
+}
---
+++ mychat/mychat/mychatApp.swift
@@ -0,0 +1,17 @@
+//
+//  mychatApp.swift
+//  mychat
+//
+//  Created by Jose Morales on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct mychatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
