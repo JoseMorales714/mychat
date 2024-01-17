7657462ebe4552f43396d23f59774b74cefc59cd
Jose Morales <moralesjose1428@gmail.com>
2024-01-16 18:52:17 -0800

    added firebase dep -> firebase ops

---
+++ mychat/mychat/Managers/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  mychat
+//
+//  Created by Jose Morales on 1/16/24.
+//
+
+import Foundation
---
+++ mychat/mychat/Managers/DatabaseManger.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManger.swift
+//  mychat
+//
+//  Created by Jose Morales on 1/16/24.
+//
+
+import Foundation
