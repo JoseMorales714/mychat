029ea53a0302911203b33a0c2d764e4e3b15cff8
Jose Morales <moralesjose1428@gmail.com>
2024-01-16 18:33:23 -0800

    UI for Sign in / Register buttons -> firebase backend

---
+++ mychat/mychat/Views/SignInScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SignInScreen.swift
+//  mychat
+//
+//  Created by Jose Morales on 1/16/24.
+//
+
+import SwiftUI
+
+struct SignInScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInScreen()
+}
