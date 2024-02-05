f93739de63e8187684366a807abb7849520e15d2
Jose Morales <moralesjose1428@gmail.com>
2024-02-05 15:29:05 -0800

    apple sign in work

--- mychat/mychat/Managers/Auth/AuthManager.swift
+++ mychat/mychat/Managers/Auth/AuthManager.swift
@@ -9,7 +9,6 @@ import Foundation
 import FirebaseAuth
 
 // y
-import CryptoKit
 import AuthenticationServices
 
 
@@ -36,11 +35,8 @@ final class AuthManager: NSObject {
     static let shared = AuthManager()
     
     let auth =  Auth.auth()
+    let signInApple = SignInApple()
     
-    // apple nonce
-    private var currentNonce: String?
-    
-    private var completionHandler: ((Result<chatUser, AppleSignInError>) -> Void)? = nil
     
     func getCurrentUser() -> chatUser? {
         guard let authUser = auth.currentUser else{
@@ -52,61 +48,35 @@ final class AuthManager: NSObject {
     
     func signInWithApple(completion: @escaping (Result<chatUser, AppleSignInError>) -> Void){
         
-        let nonce = randomNonceString()
-        currentNonce = nonce
-        completionHandler = completion
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        request.requestedScopes = [.fullName, .email]
-        request.nonce = sha256(nonce)
         
-        guard let topVC = UIApplication.getTopViewController() else{
-            completion(.failure(.unableToGrabTopVC))
-            return
+        signInApple.initAppleSignIn{ [weak self] result in
+            
+            guard let strongSelf = self else { return }
+            
+            switch result {
+            case .success(let appleResult):
+                // Initialize a Firebase credential, including the user's full name.
+                let credential = OAuthProvider.appleCredential(withIDToken: appleResult.idToken,
+                                                               rawNonce: appleResult.nonce,
+                                                               fullName: appleResult.fullname)
+                // Sign in with Firebase.
+                strongSelf.auth.signIn(with: credential) { (authResult, error) in
+                    guard let authResult = authResult, error == nil else{
+                        completion(.failure(.authSignInError))
+                        return
+                    }
+                    
+                    let user = chatUser(uid: authResult.user.uid, name: authResult.user.displayName ?? "not yet", email: authResult.user.email, photoUrl: authResult.user.photoURL?.absoluteString)
+                    completion(.success(user))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
         }
+        
         
 
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = self
-        authorizationController.presentationContextProvider = self
-        authorizationController.performRequests()
-        
     }
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-          guard let nonce = currentNonce, let completionHandler = completionHandler else {
-            fatalError("Invalid state: A login callback was received, but no login request was sent.")
-          }
-            guard let appleIDToken = appleIDCredential.identityToken, let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-            print("Unable to fetch identity token")
-            return
-          }
-          guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-            return
-          }
-          // Initialize a Firebase credential, including the user's full name.
-          let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
-                                                            rawNonce: nonce,
-                                                            fullName: appleIDCredential.fullName)
-          // Sign in with Firebase.
-          Auth.auth().signIn(with: credential) { (authResult, error) in
-              guard let authResult = authResult, error == nil else{
-                  completionHandler(.failure(.authSignInError))
-                  return
-              }
-              
-              let user = chatUser(uid: authResult.user.uid, name: authResult.user.displayName ?? "not yet", email: authResult.user.email, photoUrl: authResult.user.photoURL?.absoluteString)
-              completionHandler(.success(user))
-          }
-        }
-      }
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        // Handle error.
-        print("Sign in with Apple errored: \(error)")
-      }
     
     func signOut() throws{
         try auth.signOut()
@@ -114,45 +84,7 @@ final class AuthManager: NSObject {
 
 }
 
-extension AuthManager: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding{
-    
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return ASPresentationAnchor(frame: .zero)
-    }
-    
-    private func randomNonceString(length: Int = 32) -> String {
-      precondition(length > 0)
-      var randomBytes = [UInt8](repeating: 0, count: length)
-      let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
-      if errorCode != errSecSuccess {
-        fatalError(
-          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-        )
-      }
 
-      let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-
-      let nonce = randomBytes.map { byte in
-        // Pick a random character from the set, wrapping around if needed.
-        charset[Int(byte) % charset.count]
-      }
-
-      return String(nonce)
-    }
-
-    @available(iOS 13, *)
-    private func sha256(_ input: String) -> String {
-      let inputData = Data(input.utf8)
-      let hashedData = SHA256.hash(data: inputData)
-      let hashString = hashedData.compactMap {
-        String(format: "%02x", $0)
-      }.joined()
-
-      return hashString
-    }
-    
-}
 
 extension UIViewController: ASAuthorizationControllerPresentationContextProviding {
     public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
@@ -175,3 +107,5 @@ extension UIApplication{
         return base
     }
 }
+
+
